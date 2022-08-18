dbe8a72c065adbeed65edf6ffbf29ce04eba85f0
Sarath kumar Reddy <sarathkumarreddy@Saraths-MBP.lan>
2022-08-18 09:48:11 -0500

    initial commit

---
+++ build/precache-manifest.9fc4f77b47cca463c9ce9c24ad6ab2f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba6f5a6443380df05e0e0a349f7d4d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3a48cd2834927a9dd9a",
+    "url": "/static/css/main.3811a24b.chunk.css"
+  },
+  {
+    "revision": "2ca9e84dba565170d33b",
+    "url": "/static/js/2.714ac439.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.714ac439.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3a48cd2834927a9dd9a",
+    "url": "/static/js/main.108109c5.chunk.js"
+  },
+  {
+    "revision": "f4de7d0a9c256b025936",
+    "url": "/static/js/runtime-main.47b6b39e.js"
+  }
+]);
